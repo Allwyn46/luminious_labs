629c1a1a2001565abcbb9f72f82def4bdb438dba
Allwyn46 <allwyn153@gmail.com>
2025-10-17 20:05:19 +0530

    Added animations

--- src/app/components/hero/hero.ts
+++ src/app/components/hero/hero.ts
@@ -1,11 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { fadeInOnEnter, fadeInUpOnEnter } from '@ngverse/motion/animatecss';
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './hero.html',
-  styleUrl: './hero.css'
+  styleUrl: './hero.css',
+  animations: [
+    // pass options if supported by the helper
+    fadeInUpOnEnter({ duration: 600, delay: 100 }),
+  ],
 })
-export class Hero {
-
-}
+export class Hero {}
